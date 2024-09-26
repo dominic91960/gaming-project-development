56b6ae888b089242c920124a8edfda8612293659
Dominic Brian <dominic91960@gmail.com>
2024-09-26 16:38:35 +0530

    Improved the UI of Edit Role modal in Roles Page.

--- app/admin/components/pages/Users/components/EditRoleModal.tsx
+++ app/admin/components/pages/Users/components/EditRoleModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { IoClose } from "react-icons/io5";
+
 interface EditRoleModalProps {
   currentRole: string;
   setShowModal: (show: boolean) => void;
@@ -22,29 +24,34 @@ const EditRoleModal: React.FC<EditRoleModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded shadow-lg">
-        <h2 className="text-lg font-bold mb-4">Edit Role</h2>
+    <div className="fixed inset-0 bg-black/80 flex justify-center items-center font-medium text-[13px]">
+      <div className="relative bg-gradient-to-tr from-black from-15% to-[#0D6D49] p-[3em] rounded-md border border-[#19D38E]">
+        <button
+          className="absolute top-[1em] right-[1em] text-[#00FFA1] text-[18px]"
+          onClick={() => setShowModal(false)}
+        >
+          <IoClose />
+        </button>
+        <h2 className="font-bold text-[20px] pb-[0.6em] border-b border-b-[#0D6D49]">
+          Edit Role
+        </h2>
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={newRole}
-            onChange={(e) => setNewRole(e.target.value)}
-            className="border p-2 rounded w-full mb-4"
-            required
-          />
-          <div className="flex justify-end">
-            <button
-              type="button"
-              className="mr-2 bg-gray-300 px-4 py-2 rounded"
-              onClick={() => setShowModal(false)}
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
+          <div className="my-[2.5em]">
+            <p className="mb-[0.5em]">Name</p>
+            <input
+              type="text"
+              value={newRole}
+              onChange={(e) => setNewRole(e.target.value)}
+              className="border border-[#D9D9D9] rounded w-full bg-transparent px-[1em] py-[0.5em] outline-none"
+              required
+            />
+          </div>
+          <div className="flex gap-x-[15%] justify-between">
+            <p className="text-[10px]">
+              Please review and ensure that all the details you have entered are
+              correct before submitting.
+            </p>
+            <button className="text-black font-semibold text-[14px] px-[1.5em] py-[0.5em] bg-[#0BDB45] rounded">
               Save
             </button>
           </div>
