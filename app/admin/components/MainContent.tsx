0c6512cfa1121f161fd79c43ebb532d1205c1a96
diduladishan <diduladishan14@gmail.com>
2024-09-27 05:40:17 +0530

    resolve deployment errors

--- app/admin/components/MainContent.tsx
+++ app/admin/components/MainContent.tsx
@@ -1,30 +1,29 @@
-// components/MainContent.tsx
-import React from "react";
-import AllUsers from "./pages/Users/AllUsers";
-import Role from "./pages/Users/Roles";
+// import React from "react";
+// import AllUsers from "./pages/Users/AllUsers";
+// import Role from "./pages/Users/Roles";
 
-interface MainContentProps {
-  content: string;
-}
+// interface MainContentProps {
+//   content: string;
+// }
 
-const MainContent: React.FC<MainContentProps> = ({ content }) => {
-  return (
-    <div className="flex-1 p-10 bg-gray-100 h-screen">
-      {/* {content === "all-users" && <AllUsers />}
-      {content === "role" && <Role />}
-      {content === "profile" && <Profile />}
-      {content === "attendance-board" && (
-        <p>Hello! This is Attendance Board page</p>
-      )}
-      {content === "user-details" && <p>Hello! This is User Details page</p>}
-      {content === "all-products" && <p>Hello! This is All Products page</p>}
-      {content === "add-new" && <p>Hello! This is Add New Product page</p>}
-      {content === "categories" && <p>Hello! This is Categories page</p>}
-      {content === "tags" && <p>Hello! This is Tags page</p>}
-      {content === "brands" && <p>Hello! This is Brands page</p>}
-      {content === "customers" && <p>Hello! This is Customers page</p>} */}
-    </div>
-  );
-};
+// const MainContent: React.FC<MainContentProps> = ({ content }) => {
+//   return (
+//     <div className="flex-1 p-10 bg-gray-100 h-screen">
+//       {content === "all-users" && <AllUsers />}
+//       {content === "role" && <Role />}
+//       {content === "profile" && <Profile />}
+//       {content === "attendance-board" && (
+//         <p>Hello! This is Attendance Board page</p>
+//       )}
+//       {content === "user-details" && <p>Hello! This is User Details page</p>}
+//       {content === "all-products" && <p>Hello! This is All Products page</p>}
+//       {content === "add-new" && <p>Hello! This is Add New Product page</p>}
+//       {content === "categories" && <p>Hello! This is Categories page</p>}
+//       {content === "tags" && <p>Hello! This is Tags page</p>}
+//       {content === "brands" && <p>Hello! This is Brands page</p>}
+//       {content === "customers" && <p>Hello! This is Customers page</p>}
+//     </div>
+//   );
+// };
 
-export default MainContent;
+// export default MainContent;
