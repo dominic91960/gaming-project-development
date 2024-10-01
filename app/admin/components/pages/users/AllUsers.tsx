e410ba0007566431913979d35d7e4f60ef4c7873
Dominic Brian <dominic91960@gmail.com>
2024-10-01 14:02:49 +0530

    Styled the All Users page.

--- app/admin/components/pages/users/AllUsers.tsx
+++ app/admin/components/pages/users/AllUsers.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import AddUserModal from "../users/components/AddUserModal";
-import UserTable from "../users/components/UserTable";
+
 import axiosInstance from "@/axios/axiosInstance";
 import toast from "react-hot-toast";
 import Spinner from "@/components/Spinner/Spinner";
+import { Button } from "@/components/ui/button";
+import { CiSearch } from "react-icons/ci";
+
+import AddUserModal from "../users/components/AddUserModal";
+import UserTable from "../users/components/UserTable";
 
 const UsersPage = () => {
   const [users, setUsers] = useState([]); // Store all users
@@ -31,7 +35,11 @@ const UsersPage = () => {
   }, [currentPage, debouncedSearchTerm, selectedRole]);
 
   // Fetch all users with pagination and optional search term and role filter
-  const getUsers = async (page: number, search: string = "", role: string = "") => {
+  const getUsers = async (
+    page: number,
+    search: string = "",
+    role: string = ""
+  ) => {
     setLoading(true);
     try {
       const roleFilter = role ? `&roleName=${role}` : ""; // Add role filter if selected
@@ -90,8 +98,8 @@ const UsersPage = () => {
       buttons.push(
         <div
           key={i}
-          className={`cursor-pointer p-2 rounded-full ${
-            i === currentPage ? "bg-blue-700" : "bg-blue-500"
+          className={`cursor-pointer font-medium px-[1em] py-[0.5em] rounded-sm ${
+            i === currentPage ? "bg-[#00FFA1]" : "bg-white"
           }`}
           onClick={() => setCurrentPage(i)}
         >
@@ -107,45 +115,62 @@ const UsersPage = () => {
   }
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">All Users</h1>
-
-      <div className="flex justify-between">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-          onClick={() => {
-            setEditingUser(null);
-            setShowModal(true);
-          }}
-        >
-          Add User
-        </button>
-        {/* Search Bar */}
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search by username or email"
-          className="border p-2 mb-4 w-3/4"
-        />
-        {/* Role Selector */}
-        <select
-          value={selectedRole}
-          onChange={handleRoleChange}
-          className="border p-2 mb-4 text-black"
-        >
-          <option value="">All Roles</option>
-          <option value="ADMIN">Admin</option>
-          <option value="USER">User</option>
-          <option value="SUPER_ADMIN">Super Admin</option>
-        </select>
+    <div className="font-primaryFont text-[24px] p-[3.5em] pb-[1.5em] flex flex-col backdrop-blur-md">
+      <div className="pb-[2em] capitalize">
+        <h1 className="font-bold text-[36px] leading-none">All users</h1>
+        <p className="text-[12px]">Users/ all users</p>
       </div>
 
-      <UserTable
-        users={allAdmins}
-        deleteUser={deleteUser}
-        openEditModal={openEditModal}
-      />
+      <div className="flex-grow bg-black/40 border border-[#0D6D49] px-[2em] py-[1.2em] rounded-md">
+        <div className="flex justify-between items-center pb-[1.2em] border-b border-b-[#0D6D49]">
+          <h2 className="font-semibold capitalize">Users</h2>
+          <div className="flex items-center text-[12px] gap-x-[1em]">
+            <div className="border p-[0.75em] rounded-sm flex items-center gap-x-[0.75em]">
+              <CiSearch className="text-[1.6em]" />
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search by username or email"
+                className="bg-transparent outline-none border-s px-[1em] w-[38ch]"
+              />
+            </div>
+            <select
+              value={selectedRole}
+              onChange={handleRoleChange}
+              className="px-[1.6em] py-[0.8em] bg-transparent border rounded-sm"
+            >
+              <option value="" className="bg-white text-black">
+                All Roles
+              </option>
+              <option value="ADMIN" className="bg-white text-black">
+                Admin
+              </option>
+              <option value="USER" className="bg-white text-black">
+                User
+              </option>
+              <option value="SUPER_ADMIN" className="bg-white text-black">
+                Super Admin
+              </option>
+            </select>
+            <button
+              className="bg-[#00FFA1] font-bold text-black text-[11px] capitalize px-[2em] py-[1em] rounded hover:opacity-90 transition-opacity duration-100"
+              onClick={() => {
+                setEditingUser(null);
+                setShowModal(true);
+              }}
+            >
+              Add user
+            </button>
+          </div>
+        </div>
+        <UserTable
+          users={allAdmins}
+          deleteUser={deleteUser}
+          openEditModal={openEditModal}
+        />
+      </div>
+
       {showModal && (
         <AddUserModal
           addUser={addUser}
@@ -156,9 +181,26 @@ const UsersPage = () => {
       )}
 
       {/* Pagination */}
-      <div className="flex w-full justify-center mt-4">
-        <div className="flex text-white text-center space-x-4">
+      <div className="flex items-center justify-between text-[15px] px-[4em] mt-[2em]">
+        <div className="flex text-black text-center gap-x-[1em]">
           {renderPagination()}
+        </div>
+        <div className="flex gap-x-[1em]">
+          <p className="font-medium px-[1em] py-[0.5em] bg-white text-black rounded-md min-w-[12ch] text-center">
+            Selected: 0
+          </p>
+          <Button
+            variant="secondary"
+            className="font-medium w-[12ch] text-[1em] h-fit"
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="destructive"
+            className="font-medium w-[12ch] text-[1em] h-fit"
+          >
+            Delete
+          </Button>
         </div>
       </div>
     </div>
@@ -166,4 +208,3 @@ const UsersPage = () => {
 };
 
 export default UsersPage;
-
--- app/admin/components/pages/users/components/AddUserModal.tsx
+++ app/admin/components/pages/users/components/AddUserModal.tsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useRoleContext } from "../../../../../../context/RoleContext";
+import Image from "next/image";
+
 import axiosInstance from "@/axios/axiosInstance";
 import toast from "react-hot-toast";
+import { IoClose } from "react-icons/io5";
+import { FaCirclePlus } from "react-icons/fa6";
+import { Input } from "@/components/ui/input";
+
+import { useRoleContext } from "../../../../../../context/RoleContext";
+import samplePic from "@/public/images/sample-pic.png";
 
 interface AddUserModalProps {
   addUser: (newUser: {
@@ -39,8 +46,8 @@ const AddUserModal: React.FC<AddUserModalProps> = ({
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
   const [image, setImage] = useState("");
-  const [password, setPassword] = useState(""); 
-  const [newImageUrl, setNewImageUrl] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [newImageUrl, setNewImageUrl] = useState("");
 
   const { roles } = useRoleContext();
 
@@ -71,13 +78,11 @@ const AddUserModal: React.FC<AddUserModalProps> = ({
     try {
       let response;
       if (editingUser) {
-
         response = await axiosInstance.patch(
           `/auth/${editingUser.id}`,
           newUser
         );
       } else {
-
         response = await axiosInstance.post("/auth/register", newUser);
       }
       toast.success(response.data.message);
@@ -101,11 +106,11 @@ const AddUserModal: React.FC<AddUserModalProps> = ({
       const data = {
         fileType: fileType,
       };
-    
+
       const res = await axiosInstance.post(`presigned-url/generate`, data);
       const uploadUrl = res.data.uploadUrl;
       const downloadUrl = res.data.downloadUrl;
-  
+
       try {
         const response = await fetch(uploadUrl, {
           method: "PUT",
@@ -114,7 +119,7 @@ const AddUserModal: React.FC<AddUserModalProps> = ({
           },
           body: file,
         });
-  
+
         if (response.ok) {
           console.log("File uploaded successfully", downloadUrl);
           setNewImageUrl(downloadUrl);
@@ -140,101 +145,260 @@ const AddUserModal: React.FC<AddUserModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-8 rounded text-black">
-        <h2 className="text-xl mb-4">
-          {editingUser ? "Edit User" : "Add User"}
-        </h2>
-        {editingUser ? (
-          <div className="border p-2 mb-4 w-full">{username}</div>
-        ) : (
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="border p-2 mb-4 w-full"
-            placeholder="Enter username"
-            required
-          />
-        )}
-
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          className="border p-2 mb-4 w-full"
-          placeholder="Enter first name"
-          required
-        />
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          className="border p-2 mb-4 w-full"
-          placeholder="Enter last name"
-          required
-        />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 mb-4 w-full"
-          placeholder="Enter email"
-          required
-        />
-        {!editingUser && (
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-2 mb-4 w-full"
-            placeholder="Enter password"
-            required={!editingUser}
-          />
-        )}
-        <select
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-          className="border p-2 mb-4 w-full"
-          required
-        >
-          <option value="">Select Role</option>
-          {roles.map((role) => (
-            <option key={role.id} value={role.name}>
-              {role.name}
-            </option>
-          ))}
-        </select>
-        
-        <input
-          type="file"
-          onChange={handleImageChange}
-          className="border p-2 mb-4 w-full"
-          accept="image/*"
-        />
-
-        {image && (
-          <div className="mb-4">
-            <img src={image} alt="Profile Preview" className="w-20 h-20 mb-2 rounded-full" />
-          </div>
-        )}
-
-        <div className="flex justify-end">
+    <div className="fixed h-screen inset-0 bg-black/80 flex justify-center items-center font-medium text-[13px]">
+      <div className="relative bg-gradient-to-tr from-black/40 from-15% to-[#00a76966] backdrop-blur-sm p-[3em] rounded-md border border-[#19D38E]">
+        <div className="font-bold text-[20px] pb-[0.6em] border-b border-b-[#0D6D49] flex justify-between">
+          <h2>{editingUser ? "Edit User" : "Add User"}</h2>
           <button
-            className="bg-red-500 text-white px-4 py-2 mr-2 rounded"
+            className="text-[#00FFA1] text-[18px] hover:opacity-80 transition-opacity duration-100"
             onClick={() => setShowModal(false)}
           >
-            Cancel
+            <IoClose />
           </button>
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded"
-            onClick={handleSubmit}
-          >
-            {editingUser ? "Update" : "Save"}
-          </button>
+        </div>
+        <div className="flex gap-x-[2em] my-[2em] *:bg-black/40 *:p-[2.8em] *:rounded-sm *:border *:border-[#0D6D49]">
+          {/* Image area */}
+          <div className="flex flex-col items-center text-center mb-[15%]">
+            <div className="relative">
+              <Image
+                src={image ? image : samplePic}
+                alt="Sample pic"
+                className="size-[8em] rounded-full"
+                width={104}
+                height={104}
+              />
+              <label
+                htmlFor="profile-image"
+                className="text-[2em] text-[#0BDB45] absolute bottom-0 right-0 cursor-pointer"
+              >
+                <FaCirclePlus />
+              </label>
+            </div>
+            <div className="py-[1em] border-b border-[#0D6D49] font-secondaryFont text-[1.2em] font-bold">
+              <p>
+                {firstName !== "" || lastName !== ""
+                  ? `${firstName} ${lastName}`
+                  : "Your Name"}
+              </p>
+              <p className="font-light text-[0.6em]">
+                {email ? email : "sample@sample-domain.com"}
+              </p>
+            </div>
+          </div>
+          {/* Form area */}
+          <div>
+            <h2 className="font-bold text-[1.4em] uppercase mb-[0.5em]">
+              Personal details
+            </h2>
+
+            {/* First name and last name */}
+            <div className="grid grid-cols-2 gap-x-[4.8em] mt-[1.4em] font-medium">
+              <div>
+                <p className="capitalize">First Name</p>
+                <Input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="text-[1em] px-[1em] py-[0.6em] h-fit"
+                  required
+                />
+              </div>
+              <div>
+                <p className="capitalize">Last Name</p>
+                <Input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  className="text-[1em] px-[1em] py-[0.6em] h-fit"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Username and role */}
+            <div className="grid grid-cols-2 gap-x-[4.8em] mt-[1.4em] font-medium">
+              <div>
+                <p className="capitalize">Username</p>
+                <Input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="text-[1em] px-[1em] py-[0.6em] h-fit"
+                  readOnly={editingUser ? true : false}
+                  required
+                />
+              </div>
+              <div>
+                <p className="capitalize">Role</p>
+                <select
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                  className="bg-transparent w-full h-fit text-[1em] px-[1em] py-[0.65em] border rounded-sm"
+                  required
+                >
+                  <option value="" className="bg-black text-white">
+                    Select Role
+                  </option>
+                  {roles.map((role) => (
+                    <option
+                      key={role.id}
+                      value={role.name}
+                      className="bg-black text-white"
+                    >
+                      {role.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            {/* Email */}
+            <div className=" mt-[1.4em]">
+              <p className="capitalize">E-mail</p>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-[72ch] text-[1em] px-[1em] py-[0.6em] h-fit"
+                required
+              />
+            </div>
+
+            {/* Password */}
+            {!editingUser && (
+              <div className=" mt-[1.4em]">
+                <p className="capitalize">Password</p>
+                <Input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-[72ch] text-[1em] px-[1em] py-[0.6em] h-fit"
+                  required={!editingUser}
+                />
+              </div>
+            )}
+
+            {/* Avatar */}
+            <Input
+              id="profile-image"
+              type="file"
+              onChange={handleImageChange}
+              className="hidden"
+              accept="image/*"
+            />
+
+            {/* Submit */}
+            <div className="flex justify-between items-center mt-[18em]">
+              <p className="text-[10px] max-w-[65ch]">
+                Please review and ensure that all the details you have entered
+                are correct before submitting.
+              </p>
+              <button
+                className="text-black font-semibold text-[14px] px-[1.5em] py-[0.5em] bg-[#00FFA1] rounded hover:opacity-90 transition-opacity duration-100"
+                onClick={handleSubmit}
+              >
+                {editingUser ? "Update" : "Save"}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    // <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+    //   <div className="bg-white p-8 rounded text-black">
+    //     <h2 className="text-xl mb-4">
+    //       {editingUser ? "Edit User" : "Add User"}
+    //     </h2>
+    //     {editingUser ? (
+    //       <div className="border p-2 mb-4 w-full">{username}</div>
+    //     ) : (
+    //       <input
+    //         type="text"
+    //         value={username}
+    //         onChange={(e) => setUsername(e.target.value)}
+    //         className="border p-2 mb-4 w-full"
+    //         placeholder="Enter username"
+    //         required
+    //       />
+    //     )}
+
+    //     <input
+    //       type="text"
+    //       value={firstName}
+    //       onChange={(e) => setFirstName(e.target.value)}
+    //       className="border p-2 mb-4 w-full"
+    //       placeholder="Enter first name"
+    //       required
+    //     />
+    //     <input
+    //       type="text"
+    //       value={lastName}
+    //       onChange={(e) => setLastName(e.target.value)}
+    //       className="border p-2 mb-4 w-full"
+    //       placeholder="Enter last name"
+    //       required
+    //     />
+    //     <input
+    //       type="email"
+    //       value={email}
+    //       onChange={(e) => setEmail(e.target.value)}
+    //       className="border p-2 mb-4 w-full"
+    //       placeholder="Enter email"
+    //       required
+    //     />
+    //     {!editingUser && (
+    //       <input
+    //         type="password"
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //         className="border p-2 mb-4 w-full"
+    //         placeholder="Enter password"
+    //         required={!editingUser}
+    //       />
+    //     )}
+    //     <select
+    //       value={role}
+    //       onChange={(e) => setRole(e.target.value)}
+    //       className="border p-2 mb-4 w-full"
+    //       required
+    //     >
+    //       <option value="">Select Role</option>
+    //       {roles.map((role) => (
+    //         <option key={role.id} value={role.name}>
+    //           {role.name}
+    //         </option>
+    //       ))}
+    //     </select>
+
+    //     <input
+    //       type="file"
+    //       onChange={handleImageChange}
+    //       className="border p-2 mb-4 w-full"
+    //       accept="image/*"
+    //     />
+
+    //     {image && (
+    //       <div className="mb-4">
+    //         <img src={image} alt="Profile Preview" className="w-20 h-20 mb-2 rounded-full" />
+    //       </div>
+    //     )}
+
+    //     <div className="flex justify-end">
+    //       <button
+    //         className="bg-red-500 text-white px-4 py-2 mr-2 rounded"
+    //         onClick={() => setShowModal(false)}
+    //       >
+    //         Cancel
+    //       </button>
+    //       <button
+    //         className="bg-blue-500 text-white px-4 py-2 rounded"
+    //         onClick={handleSubmit}
+    //       >
+    //         {editingUser ? "Update" : "Save"}
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
