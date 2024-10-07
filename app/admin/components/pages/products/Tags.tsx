15bab19a14367e31ccc0aea854b0f94f02643df2
Dominic Brian <dominic91960@gmail.com>
2024-10-07 09:12:04 +0530

    Improved the responsiveness of the Tags Page.

--- app/admin/components/pages/products/Tags.tsx
+++ app/admin/components/pages/products/Tags.tsx
@@ -60,12 +60,14 @@ export default function TagsPage() {
   };
 
   return (
-    <div className="min-h-full font-primaryFont text-[24px] p-[3.5em] pb-[1.5em] flex flex-col backdrop-blur-md text-white">
-      <div className="pb-[2em]">
-        <h1 className="font-bold text-[36px] leading-none text-white">
+    <div className="min-h-full font-primaryFont text-[8px] sm:text-[12px] md:text-[16px] xl:text-[20px] 2xl:text-[24px] pt-[3.5em] md:p-[3.5em] pb-[1.5em] flex flex-col backdrop-blur-[2px] text-white">
+      <div className="pb-[2em] px-[36px]">
+        <h1 className="font-bold text-[1.5em] leading-none text-white">
           All Tags
         </h1>
-        <p className="text-[12px] text-white">Products / Tags</p>
+        <p className="text-[0.9em] text-white md:text-[0.5em]">
+          Products / Tags
+        </p>
       </div>
 
       <DataTable
@@ -74,7 +76,6 @@ export default function TagsPage() {
         onDelete={handleDelete}
         onAddTags={handleAddTags}
       />
-
       <EditTagPopup
         isOpen={isEditPopupOpen}
         onClose={() => setEditPopupOpen(false)}
--- app/admin/components/pages/products/tags/data-table.tsx
+++ app/admin/components/pages/products/tags/data-table.tsx
@@ -63,10 +63,29 @@ export function DataTable<TData, TValue>({
 
   return (
     <>
-      <div className="flex-grow bg-black/40 border border-[#0D6D49] px-[2em] py-[1.2em] rounded-md text-white">
-        <div className="flex justify-between items-center pb-[1.2em] border-b border-b-[#0D6D49]">
-          <h2 className="font-semibold text-[24px] text-white">Tags</h2>
-          <div className="flex items-center text-[12px] gap-x-[1em]">
+      {/* Mobile search bar and add menu */}
+      <div className="flex items-center justify-center px-[36px] mb-[2em] gap-x-[1em] md:hidden">
+        <div className="border p-[0.75em] rounded-sm flex items-center gap-x-[0.75em] w-full">
+          <CiSearch className="text-[1.6em] text-white" />
+          <input
+            placeholder="Filter names..."
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+            className="bg-transparent outline-none border-y-0 border-e-0 border-s rounded-none px-[1em] w-full text-white md:w-[38ch]"
+          />
+        </div>
+        <AddTagsPop onAddTags={onAddTags} />
+      </div>
+
+      <div className="flex-grow bg-black/40 border border-[#0D6D49] px-[2em] py-[1.2em] rounded-3xl md:rounded-md text-white">
+        <div className="hidden pb-[1.2em] border-b border-b-[#0D6D49] md:flex md:justify-between md:items-center">
+          {/* Title */}
+          <h2 className="font-semibold text-white">Tags</h2>
+
+          {/* Search bar and add menu */}
+          <div className="flex items-center text-[0.5em] gap-x-[1em]">
             <div className="border p-[0.75em] rounded-sm flex items-center gap-x-[0.75em]">
               <CiSearch className="text-[1.6em] text-white" />
               <input
@@ -80,13 +99,14 @@ export function DataTable<TData, TValue>({
                 className="bg-transparent outline-none border-y-0 border-e-0 border-s rounded-none px-[1em] w-[38ch] text-white"
               />
             </div>
-
             <AddTagsPop onAddTags={onAddTags} />
           </div>
         </div>
-        <div className="mt-[1.5em] text-white">
-          <Table className="border-separate border-spacing-y-[0.8em] px-[0.4em]">
-            <TableHeader>
+
+        {/* Table */}
+        <div className="text-white md:mt-[1.5em]">
+          <Table className="border-separate border-spacing-y-[2em] px-[0.4em] text-[0.65em] md:border-spacing-y-[1em]">
+            <TableHeader className="text-[1.1em]">
               {table.getHeaderGroups().map((headerGroup) => (
                 <TableRow
                   key={headerGroup.id}
@@ -94,7 +114,10 @@ export function DataTable<TData, TValue>({
                 >
                   {headerGroup.headers.map((header) => {
                     return (
-                      <TableHead key={header.id}>
+                      <TableHead
+                        key={header.id}
+                        className="hidden md:table-cell h-fit py-[1em]"
+                      >
                         {header.isPlaceholder
                           ? null
                           : flexRender(
@@ -113,18 +136,20 @@ export function DataTable<TData, TValue>({
                   <TableRow
                     key={row.id}
                     data-state={row.getIsSelected() && "selected"}
-                    className="bg-white/5 hover:bg-white/5 cursor-pointer hover:shadow-[0px_0px_5px_#00FFA1] rounded-sm"
+                    className="bg-white/5 hover:bg-white/5 cursor-pointer hover:shadow-[0px_0px_5px_#00FFA1] rounded-sm text-[2em] sm:text-[1.4em] md:text-[1em]"
                   >
                     {row.getVisibleCells().map((cell, index) => (
                       <TableCell
                         key={cell.id}
-                        className={
+                        className={`py-[1.3em] px-[0.2em] ${
                           index === 0
-                            ? "relative rounded-s-sm"
+                            ? "relative rounded-s-sm ps-[1.2em]"
+                            : index === 1
+                            ? "max-w-[16ch] text-nowrap overflow-hidden text-ellipsis pe-[1em]"
                             : index === 3
                             ? "rounded-e-sm"
                             : ""
-                        }
+                        }`}
                       >
                         {index === 0 && (
                           <div className="w-[0.3em] h-full bg-[#00FFA1] absolute top-0 left-0 rounded-full"></div>
@@ -148,25 +173,48 @@ export function DataTable<TData, TValue>({
           </Table>
         </div>
       </div>
-      <div className="flex items-center space-x-2 py-4">
-        <Button
-          variant="secondary"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-          className="hover:bg-[#00FFA1]"
-        >
-          Previous
-        </Button>
-        <Button
-          variant="secondary"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-          className="hover:bg-[#00FFA1]"
-        >
-          Next
-        </Button>
+
+      {/* Pagination */}
+      <div className="text-[0.65em] px-[4em] mt-[2em] hidden md:flex md:items-center md:justify-between">
+        <div className="flex gap-x-[1em]">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+            className="text-[1em] hover:bg-[#00FFA1] px-[1em] py-[0.5em] h-fit rounded-sm"
+          >
+            Previous
+          </Button>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+            className="text-[1em] hover:bg-[#00FFA1] px-[1em] py-[0.5em] h-fit rounded-sm"
+          >
+            Next
+          </Button>
+        </div>
+
+        {/* Group select */}
+        {/* <div className="flex gap-x-[1em]">
+          <p className="font-medium px-[1em] py-[0.5em] bg-white text-black rounded-sm min-w-[12ch] flex items-center justify-center h-fit">
+            Selected: 0
+          </p>
+          <Button
+            variant="secondary"
+            className="font-medium w-[12ch] text-[1em] px-[1em] py-[0.5em] h-fit rounded-sm"
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="destructive"
+            className="font-medium w-[12ch] text-[1em] px-[1em] py-[0.5em] h-fit rounded-sm"
+          >
+            Delete
+          </Button>
+        </div> */}
       </div>
     </>
   );
