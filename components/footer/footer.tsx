a662eef694f230b13c0890170d4f1b1840892bb5
Dominic Brian <dominic91960@gmail.com>
2024-10-10 07:35:38 +0530

    Minor changes in home page

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+
 import logo from "@/public/images/footer/logo.png";
 import fb from "@/public/images/footer/facebook.png";
 import x from "@/public/images/footer/x.png";
@@ -16,127 +17,151 @@ import skrill from "@/public/images/footer/skrill.png";
 
 export default function Footer() {
   return (
-    <section className="bg-[#0B0E13] text-white">
-      <div className="container mx-auto font-primaryFont pt-[3.3%] w-full px-8 justify-between">
-        <div
-          className="bg-[#2D2E37] px-8 py-4 xl:px-16   flex items-center justify-between md:gap-20 mx-auto"
-          style={{
-            clipPath:
-              "polygon(0% 0%, 98% 0%, 100% 20%, 100% 100%, 2% 100%, 0% 80%)",
-          }}
-        >
-          <h2 className="text-[8px] md:text-md md:leading-10 lg:text-lg xl:text-2xl 2xl:text-3xl font-semibold uppercase">
-            Subscribe Newsletter
-          </h2>
-          <form className="flex items-center gap-2 lg:gap-[2.5em] text-[7px] lg:text-[14px]">
-            <Input
-              type="email"
-              placeholder="Email Address"
-              className="text-white bg-[#0B0E13] border-none rounded-none px-[4px] py-[3px] h-fit  lg:w-[37ch] placeholder:text-white lg:py-[1.5em] ps-[1em] text-[7px] lg:text-[14px]"
-            />
-            <Button className="px-[4px] py-[3px] h-fit lg:px-[1.8em] lg:py-[1.4em] text-black bg-[#0BDB45] leading-none rounded-none hover:text-white hover:bg-[#108832] text-[7px] lg:text-[14px]">
-              <p className="flex gap-[0.5em] font-bold items-center ">
-                Subscribe <span className="text-xm lg:text-sm">&#10230;</span>
-              </p>
-            </Button>
-          </form>
+    <section className="bg-[#0B0E13] font-primaryFont text-white text-[7px] py-[2em]">
+      {/* main container */}
+      <div className="container mx-auto px-[36px]">
+        {/* Logo and desc*/}
+        <div className="mb-[2.86em]">
+          <div className="flex items-center justify-center gap-x-[1em] mb-[1.1em]">
+            <Image src={logo} alt="Logo" className="size-[4.45em]" />
+            <h3 className="font-semibold text-[1.7em]">LOGO</h3>
+          </div>
+          <p className="text-center px-[2.86em]">
+            Your one-stop destination for unlocking the ultimate gaming
+            experience. Our online store offers a wide range of game keys for
+            PC, consoles, and popular gaming platforms at unbeatable prices
+          </p>
         </div>
-        <div className="mt-[4.3%] flex flex-col xl:flex-row gap-6 xl:gap-[144px] text-center">
-          <div className="flex flex-col text-center lg:text-left lg:justify-start">
-            <div className="flex items-center justify-center">
-              <Image src={logo} alt="Logo" className="w-[31.05px] lg:w-[117px]" />
-              <h3 className="uppercase font-semibold text-sm lg:text-[36px] ps-[0.5em]">
-                Logo
-              </h3>
-            </div>
-            <p className="text-[7px] lg:text-[16px] mb-8">
-              Your one-stop destination for unlocking the ultimate gaming
-              experience. Our online store offers a wide range of game keys for
-              PC, consoles, and popular gaming platforms at unbeatable prices.
+
+        {/* Site links */}
+        <div className="flex justify-center gap-x-[7em] mb-[2.86em]">
+          {/* Company list */}
+          <ul className="uppercase text-[calc(1em+1px)]">
+            <h4 className="font-medium text-[1.5em]">Company</h4>
+            <li className="mt-[0.8em]">
+              <Link href="/">Products</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">Blog</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">About</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">Contact</Link>
+            </li>
+          </ul>
+
+          {/* Categories list */}
+          <ul className="uppercase text-[calc(1em+1px)] text-right">
+            <h4 className="font-medium text-[1.25em]">Categories</h4>
+            <li className="mt-[0.8em]">
+              <Link href="/">Games</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">Gift cards</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">E-money</Link>
+            </li>
+            <li className="mt-[0.8em]">
+              <Link href="/">Pre orders</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Social links */}
+        <div className="flex flex-col items-center mb-[2.86em]">
+          <h4 className="font-semibold uppercase text-[2.1em]">Follow us</h4>
+          <ul className="flex gap-x-[1.4em] mt-[0.8em]">
+            <li>
+              <Link href="/">
+                <Image src={fb} alt="Facebook" className="size-[2em]" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <Image src={x} alt="X" className="size-[2em]" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <Image src={ig} alt="Instagram" className="size-[2em]" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <Image src={tt} alt="TikTok" className="size-[2em]" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Seperator */}
+      <hr className="mb-[1.5em]" />
+
+      {/* Second container */}
+      <div className="container mx-auto px-[36px] flex flex-col font-semibold">
+        <div className="flex items-center justify-center gap-x-[0.7em] leading-[0] mb-[1.5em]">
+          <Image src={copyright} alt="Copyright" className="size-[1.5em]" />
+          <p>Copyright {new Date().getFullYear()}. All rights reserved</p>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-x-[1em]">
+          <div className="flex flex-row gap-x-[0.7em] mb-[1.5em]">
+            <Image src={paypal} alt="PayPal" className="w-[1.9em]" />
+            <Image src={visa} alt="Visa" className="w-[1.9em]" />
+            <Image src={mastercard} alt="Mastercard" className="w-[1.9em]" />
+            <Image src={skrill} alt="Skrill" className="w-[1.9em]" />
+          </div>
+          <div className="flex gap-[1em]">
+            <p>
+              <Link href="/">Privacy policy</Link>
             </p>
-          </div>
-          <div className="flex flex-row lg:flex-row space-x-10 justify-center">
-            <ul className="uppercase text-[8px] lg:text-[18px] font-light lg:font-semibold flex-shrink-0">
-              <h4 className="text-[11px] lg:text-[25px] font-semibold">Company</h4>
-              <li className="mt-[0.8em]">
-                <Link href="/">Products</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">Blog</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">About</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">Contact</Link>
-              </li>
-            </ul>
-            <ul className="uppercase text-[8px] lg:text-[18px] font-normal lg:font-semibold flex-shrink-0">
-              <h4 className="text-[11px] lg:text-[25px] font-semibold">Categories</h4>
-              <li className="mt-[0.8em]">
-                <Link href="/">Games</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">Gift cards</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">E-money</Link>
-              </li>
-              <li className="mt-[0.8em]">
-                <Link href="/">Pre orders</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="uppercase text-[7px] lg:text-[18px] font-semibold flex-shrink-0">
-            <h4 className="text-[15px] lg:text-[25px]">Follow us</h4>
-            <ul className="flex mt-[0.8em] gap-[1.6em] justify-center">
-              <li>
-                <Link href="/">
-                  <Image src={fb} alt="Link to Facebook" className="w-4" />
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <Image src={x} alt="Link to X"  className="w-4"/>
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <Image src={ig} alt="Link to Instagram" className="w-4" />
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <Image src={tt} alt="Link to TikTok" className="w-4" />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="mt-[2.7%] border-t-2 pt-[1.5%] pb-[2.5%] capitalize w-full flex flex-col lg:flex-row items-center lg:text-[15px] justify-between font-semibold text-[7px]">
-          <div className="flex items-center gap-[1.6em]">
-            <Image src={copyright} alt="Copyright" className="w-4 lg:w-8" />
-            <p>Copyright {new Date().getFullYear()}. All rights reserved</p>
-          </div>
-          <div className="flex flex-row items-center gap-2 ">
-            <div className="flex flex-row gap-2">
-              <Image src={paypal} alt="PayPal" className="w-[10px] lg:w-6" />
-              <Image src={visa} alt="Visa" className="w-[10px] lg:w-6"/>
-              <Image src={mastercard} alt="Mastercard" className="w-[10px] lg:w-6"/>
-              <Image src={skrill} alt="Skrill" className="w-[10px] lg:w-6" />
-            </div>
-            <div className="flex gap-2">
-              <p>
-                <Link href="/">Privacy policy</Link>
-              </p>
-              <p>|</p>
-              <p>
-                <Link href="/">Terms & conditions</Link>
-              </p>
-            </div>
+            <p>|</p>
+            <p>
+              <Link href="/">Terms & conditions</Link>
+            </p>
           </div>
         </div>
       </div>
     </section>
   );
+}
+
+{
+  /* Newsletter div */
+}
+{
+  /* <div
+  className="bg-[#2D2E37] flex items-center justify-between px-[2em] py-[1em]"
+  style={{
+    clipPath: "polygon(0% 0%, 98% 0%, 100% 20%, 100% 100%, 2% 100%, 0% 80%)",
+  }}
+> */
+}
+{
+  /* title */
+}
+{
+  /* <h2 className="font-semibold text-[calc(1em+1px)] uppercase">
+    Subscribe Newsletter
+  </h2> */
+}
+
+{
+  /* form */
+}
+{
+  /* <form className="flex gap-x-[calc(1em-1px)]">
+    <Input
+      type="email"
+      placeholder="Email Address"
+      className="w-[23ch] h-fit bg-[#0B0E13] text-[1em] px-[6px] py-[0.5em] border-none rounded-none"
+    />
+    <Button variant="gaming" className="h-fit text-[1em] px-[4px] py-[0.5em]">
+      Subscribe
+    </Button>
+  </form>
+</div>; */
 }
