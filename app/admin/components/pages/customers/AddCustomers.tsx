46013e415ea5096ed312408f4c22b370f53447fe
Dominic Brian <dominic91960@gmail.com>
2024-10-15 12:45:54 +0530

    Made the AddCustomer form responsive.

--- app/admin/components/pages/customers/AddCustomers.tsx
+++ app/admin/components/pages/customers/AddCustomers.tsx
@@ -6,6 +6,7 @@ import { FaCirclePlus } from "react-icons/fa6";
 
 import { AllCustomersNew } from "./columns";
 import samplePic from "@/public/images/sample-pic.png";
+import { Input } from "@/components/ui/input";
 
 interface AddCustomersProps {
   onClose: () => void;
@@ -26,6 +27,7 @@ export default function AddCustomers({
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    onClose();
 
     const newCustomer: AllCustomersNew = {
       id: Math.random().toString(36).substr(2, 9),
@@ -49,7 +51,7 @@ export default function AddCustomers({
 
   return (
     <div className="fixed h-full inset-0 bg-black/80 flex justify-center items-center font-medium text-[9px] sm:text-[10px] md:text-[11px] lg:text-[12px] xl:text-[13px] z-10">
-      <div className="relative bg-gradient-to-tr from-black/40 from-15% to-[#00a76966] backdrop-blur-[2px] p-[3em] rounded-md border border-[#19D38E]">
+      <div className="relative bg-gradient-to-tr from-black/40 from-15% to-[#00a76966] backdrop-blur-[2px] p-[3em] rounded-md border border-[#19D38E] mx-[2em] sm:mx-0">
         <div className="font-bold text-[1.5em] pb-[0.6em] border-b border-b-[#0D6D49] flex justify-between">
           <h2>Add Customer</h2>
           <button
@@ -93,21 +95,148 @@ export default function AddCustomers({
             </div>
           </div>
 
-          <form
-            onSubmit={handleSubmit}
-            className="mb-6 p-4 border rounded-md text-white"
-          >
-            <div className="mb-4">
-              <label className="block mb-1">Customer Name</label>
-              <input
+          <form onSubmit={handleSubmit}>
+            <h2 className="font-bold text-[1.4em] uppercase mb-[0.5em]">
+              Personal details
+            </h2>
+
+            {/* Full name and ID */}
+            <div className="grid grid-cols-2 gap-x-[0.7em] mt-[1.4em] font-medium xl:gap-x-[2em] 2xl:gap-x-[4.8em]">
+              <div>
+                <label className="block capitalize">Full Name</label>
+                <Input
+                  type="text"
+                  value={customer_name}
+                  onChange={(e) => setCustomer_name(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block capitalize">Customer ID</label>
+                <Input
+                  type="text"
+                  value={customer_id}
+                  onChange={(e) => setCustomer_id(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Customer username and DOB */}
+            <div className="grid grid-cols-2 gap-x-[0.7em] mt-[1.4em] font-medium xl:gap-x-[2em] 2xl:gap-x-[4.8em]">
+              <div>
+                <label className="block capitalize">Username</label>
+                <Input
+                  type="text"
+                  value={customer_username}
+                  onChange={(e) => setCustomer_username(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block capitalize">Date of birth</label>
+                <Input
+                  type="date"
+                  // value={customer_id}
+                  // onChange={(e) => setCustomer_id(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Address */}
+            <div className="mt-[1.4em]">
+              <label className="block capitalize">Address</label>
+              <Input
                 type="text"
-                value={customer_name}
-                onChange={(e) => setCustomer_name(e.target.value)}
-                className="w-full p-2 border rounded text-black"
+                className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+              />
+            </div>
+
+            {/* State, city and zip code */}
+            <div className="grid grid-cols-3 gap-x-[0.7em] mt-[1.4em] font-medium xl:gap-x-[2em] 2xl:gap-x-[4.8em]">
+              <div>
+                <label className="block capitalize">State</label>
+                <Input
+                  type="text"
+                  className="text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                />
+              </div>
+              <div>
+                <label className="block capitalize">City</label>
+                <Input
+                  type="text"
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                />
+              </div>
+              <div>
+                <label className="block capitalize">ZIP Code</label>
+                <Input
+                  type="text"
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                />
+              </div>
+            </div>
+
+            {/* Country and phone */}
+            <div className="grid grid-cols-2 gap-x-[0.7em] mt-[1.4em] font-medium xl:gap-x-[2em] 2xl:gap-x-[4.8em]">
+              <div>
+                <label className="block capitalize">Country</label>
+                <Input
+                  type="text"
+                  value={customer_country}
+                  onChange={(e) => setCustomer_country(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block capitalize">Phone</label>
+                <Input
+                  type="text"
+                  value={customer_phone}
+                  onChange={(e) => setCustomer_phone(e.target.value)}
+                  className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+                  required
+                />
+              </div>
+            </div>
+
+            {/* Email */}
+            <div className="mt-[1.4em]">
+              <label className="block capitalize">Email</label>
+              <Input
+                type="text"
+                className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+              />
+            </div>
+
+            {/* Password */}
+            <div className="mt-[1.4em]">
+              <label className="block capitalize">Password</label>
+              <Input
+                type="text"
+                className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
+              />
+            </div>
+
+            {/* Image URL */}
+            <div className="mt-[1.4em]">
+              <label className="block capitalize">Image URL</label>
+              <Input
+                type="text"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                className="w-full text-[1em] px-[1em] py-[0.6em] h-fit rounded-sm"
                 required
               />
             </div>
-            <div className="mb-4">
+
+            {/* <div className="mb-4">
               <label className="block mb-1 text-white">Customer ID</label>
               <input
                 type="text"
@@ -116,8 +245,9 @@ export default function AddCustomers({
                 className="w-full p-2 border rounded text-black"
                 required
               />
-            </div>
-            <div className="mb-4">
+            </div> */}
+
+            {/* <div className="mb-4">
               <label className="block mb-1">Customer Username</label>
               <input
                 type="text"
@@ -126,8 +256,9 @@ export default function AddCustomers({
                 className="w-full p-2 border rounded text-black"
                 required
               />
-            </div>
-            <div className="mb-4">
+            </div> */}
+
+            {/* <div className="mb-4">
               <label className="block mb-1">Country</label>
               <input
                 type="text"
@@ -136,8 +267,9 @@ export default function AddCustomers({
                 className="w-full p-2 border rounded text-black"
                 required
               />
-            </div>
-            <div className="mb-4">
+            </div> */}
+
+            {/* <div className="mb-4">
               <label className="block mb-1">Phone</label>
               <input
                 type="text"
@@ -146,9 +278,9 @@ export default function AddCustomers({
                 className="w-full p-2 border rounded text-black"
                 required
               />
-            </div>
+            </div> */}
 
-            <div className="mb-4">
+            {/* <div className="mb-4">
               <label className="block mb-1">Image URL</label>
               <input
                 type="text"
@@ -157,13 +289,28 @@ export default function AddCustomers({
                 className="w-full p-2 border rounded"
                 required
               />
-            </div>
-            <button
+            </div> */}
+
+            {/* <button
               type="submit"
               className="px-4 py-2 bg-blue-500 text-white rounded"
             >
               Save Product
-            </button>
+            </button> */}
+
+            {/* Submit */}
+            <div className="flex justify-between items-center mt-[2em]">
+              <p className="text-[0.76em] max-w-[40ch] md:max-w-[44ch] 2xl:max-w-[65ch]">
+                Please review and ensure that all the details you have entered
+                are correct before submitting.
+              </p>
+              <button
+                type="submit"
+                className="text-black font-semibold text-[1.1em] px-[1.5em] py-[0.5em] bg-[#00FFA1] rounded hover:opacity-90 transition-opacity duration-100"
+              >
+                Save
+              </button>
+            </div>
           </form>
         </div>
       </div>
