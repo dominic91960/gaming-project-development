2f90eb2403568c81a2466605149bec79d0afb689
Dominic Brian <dominic91960@gmail.com>
2024-11-06 17:06:47 +0530

    Styled order detail popup.

--- app/admin/components/pages/orders/OrderDetailPopup copy.tsx
+++ app/admin/components/pages/orders/OrderDetailPopup copy.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import OrderItemsTable from "./_components/OrderItemsTable"; // Import the new component
+import { IoClose } from "react-icons/io5";
 
 interface OrderItem {
   price: number;
@@ -29,13 +30,19 @@ const OrderDetailPopup: React.FC<OrderDetailPopupProps> = ({
 }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="bg-gradient-to-tr from-black from-15% to-[#0D6D49] p-[3em] rounded-md border border-[#19D38E] sm:w-auto">
         <DialogTitle className="text-white">{products[0]?.price}</DialogTitle>
+        <button
+          className="absolute top-[1em] right-[1em] text-[#00FFA1] text-[1.4em] hover:opacity-80 transition-opacity duration-100"
+          onClick={onClose}
+        >
+          <IoClose />
+        </button>
         <div>
           <OrderItemsTable products={products} order={order} />
-          <Button variant="outline" className="text-white" onClick={onClose}>
+          {/* <Button variant="outline" className="text-white" onClick={onClose}>
             Close
-          </Button>
+          </Button> */}
         </div>
       </DialogContent>
     </Dialog>
