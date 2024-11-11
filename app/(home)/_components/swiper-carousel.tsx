fad1dbf5a3a60b7cfeea169391d0accd3258e3d2
Dominic Brian <dominic91960@gmail.com>
2024-11-11 18:23:18 +0530

    Fixed some minor errors.

--- app/(home)/_components/swiper-carousel.tsx
+++ app/(home)/_components/swiper-carousel.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 
+import axiosInstance from "@/axios/axiosInstance";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 
@@ -9,70 +10,9 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-import cardBgOne from "@/public/images/home/swiper-carousel/card-bg-one.png";
-import cardBgTwo from "@/public/images/home/swiper-carousel/card-bg-two.png";
-import cardBgThree from "@/public/images/home/swiper-carousel/card-bg-three.png";
-import cardBgFour from "@/public/images/home/swiper-carousel/card-bg-four.png";
-import gamePoster from "@/public/images/home/swiper-carousel/poster.png";
-import SwiperCarouselCard from "./swiper-carousel-card";
-import axiosInstance from "@/axios/axiosInstance";
-import { set } from "date-fns";
 import SwiperCarouselSkeleton from "./swiper-carousel-skeleton";
-
-// const data = [
-//   {
-//     id: "670f554d92cae82274f98b54",
-//     background: cardBgOne.src,
-//     poster: gamePoster.src,
-//     title: "Warhammer 40,000: Space Marine 2",
-//     rating: 0,
-//     description:
-//       "Black Myth: Wukong is an action RPG rooted in Chinese mythology. You shall set out as the Destined One to venture into the challenges and marvels ahead, to uncover the obscured truth beneath the veil of a glorious legend from the past. ",
-//     price: 40.0,
-//     wishlistedBy: 9452399,
-//     releaseDate: "20 Aug, 2024",
-//     soldOut: true,
-//   },
-//   {
-//     id: "670f5ce892cae82274f98b5b",
-//     background: cardBgTwo.src,
-//     poster: gamePoster.src,
-//     title: "Black Myth: Wukong",
-//     rating: 5,
-//     description:
-//       "Embark on an epic journey through Ancient Greece and shape your destiny as a legendary Spartan hero.",
-//     price: 40.0,
-//     wishlistedBy: 539999,
-//     releaseDate: "20 Aug, 2024",
-//     soldOut: false,
-//   },
-//   {
-//     id: "670fbcca0a1092877a48494c",
-//     background: cardBgThree.src,
-//     poster: gamePoster.src,
-//     title: "Black Myth: Wukong",
-//     rating: 5,
-//     description:
-//       "Red Dead Redemption 2 is an epic tale of life in America at the dawn of the modern age. Set in 1899, you play as Arthur Morgan, an outlaw and a member of the Van der Linde gang, who must deal with the decline of the Wild West while surviving against government forces, rival gangs, and other adversaries in an open world setting.",
-//     price: 40.0,
-//     wishlistedBy: 24364,
-//     releaseDate: "20 Aug, 2024",
-//     soldOut: false,
-//   },
-//   {
-//     id: "67160d8b0342881a0be83757",
-//     background: cardBgFour.src,
-//     poster: gamePoster.src,
-//     title: "Black Myth: Wukong",
-//     rating: 5,
-//     description:
-//       "The Last of Us is an action/adventure game set in a post-apocalyptic world. You play as Joel, a smuggler tasked with escorting a teenage girl, Ellie, across the United States, while battling hostile humans and zombie-like creatures infected by a mutated fungus. The game focuses on stealth and combat in an emotionally driven narrative.",
-//     price: 4000.0,
-//     wishlistedBy: 1200,
-//     releaseDate: "20 Aug, 2024",
-//     soldOut: false,
-//   },
-// ];
+import SwiperCarouselCard from "./swiper-carousel-card";
+import cardBgOne from "@/public/images/home/swiper-carousel/card-bg-one.png";
 
 interface GameData {
   id: string;
@@ -89,6 +29,7 @@ interface GameData {
 
 const SwiperCarousel = () => {
   const getTopRatedGames = async () => {
+    setLoading(true);
     try {
       const response = await axiosInstance.get(
         "/games?addToCarousel=true&limit=4"
@@ -112,8 +53,7 @@ const SwiperCarousel = () => {
     }
   };
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => getTopRatedGames(), 10000);
+    getTopRatedGames();
   }, []);
 
   const [bg, setBg] = useState("");
