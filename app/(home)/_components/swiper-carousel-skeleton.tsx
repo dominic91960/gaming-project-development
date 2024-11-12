33301c998ccfb14b55257ba936545a5a84c36232
Dominic Brian <dominic91960@gmail.com>
2024-11-12 09:47:02 +0530

    Changed swiper skeleton.

--- app/(home)/_components/swiper-carousel-skeleton.tsx
+++ app/(home)/_components/swiper-carousel-skeleton.tsx
@@ -8,22 +8,22 @@ import Rating from "@/components/rating/rating";
 const SwiperCarouselSkeleton = () => {
   return (
     <article
-      className="w-full h-fit backdrop-blur-[10px] p-[5em] pt-0 mt-[8em] mb-[3em] md:p-[2em] md:mt-[4em] md:mb-[0.5em] md:grid md:grid-cols-2 md:place-items-end md:gap-x-[2em] animate-pulse"
-      style={{
-        backgroundImage: `linear-gradient(to top right, #002304 0%, #FFFFFF14 40%, #FFFFFF14 60%, #002304 100%)`,
-        borderImage: `linear-gradient(to bottom, #9DA8A0 0%, #00FF47 100%) 1`,
-        borderWidth: "1px",
-        borderStyle: "solid",
-      }}
+      className="w-full h-fit bg-white/20 backdrop-blur-[10px] p-[5em] pt-0 mt-[8em] mb-[3em] md:p-[2em] md:mt-[4em] md:mb-[0.5em] md:grid md:grid-cols-2 md:place-items-end md:gap-x-[2em] animate-pulse"
+      // style={{
+      //   backgroundImage: `linear-gradient(to top right, #002304 0%, #FFFFFF14 40%, #FFFFFF14 60%, #002304 100%)`,
+      //   borderImage: `linear-gradient(to bottom, #9DA8A0 0%, #00FF47 100%) 1`,
+      //   borderWidth: "1px",
+      //   borderStyle: "solid",
+      // }}
     >
       {/* Poster */}
-      <div className="relative w-[92%] h-[300px] bg-white/20 mx-auto -translate-y-[7em] md:w-full md:h-full md:-translate-y-0">
-        <div className="absolute left-0 bottom-0 w-full h-[318px] flex items-center justify-center text-center md:h-[328px] md:items-start lg:h-[340px] xl:h-[427px] 2xl:h-[513px]"></div>
+      <div className="relative w-[92%] h-[300px] mx-auto -translate-y-[7em] md:w-full md:h-full md:-translate-y-0">
+        <div className="absolute left-0 bottom-0 w-full h-[318px] bg-white/40 flex items-center justify-center text-center md:h-[328px] md:items-start lg:h-[340px] xl:h-[427px] 2xl:h-[513px]"></div>
       </div>
 
       <div className="-mt-[5em] md:-mt-0">
         {/* Title */}
-        <h3 className="font-bold text-[2em] line-clamp-1 uppercase opacity-20">
+        <h3 className="font-bold text-[2em] line-clamp-1 uppercase text-transparent bg-white w-1/2 leading-none mt-[0.1em] mb-[0.2em] opacity-50">
           Vingame
         </h3>
 
@@ -34,15 +34,15 @@ const SwiperCarouselSkeleton = () => {
         <hr className="w-2/5" />
 
         {/* Description */}
-        <div className="h-[15em] pt-[1.5em] text-justify font-normal opacity-20">
-          <p className="line-clamp-6">
-            Your one-stop destination for unlocking the ultimate gaming
-            experience.
+        <div className="h-[15em] pt-[1.5em] text-justify font-normal text-transparent opacity-50">
+          <p className="line-clamp-1 bg-white mb-[0.4em]">
+            Your one-stop destination.
           </p>
+          <p className="line-clamp-1 bg-white">Your one-stop destination.</p>
         </div>
 
         {/* Price and release date */}
-        <div className="flex items-end justify-between pt-[1.5em] opacity-0">
+        <div className="flex items-end justify-between pt-[1.5em] text-transparent opacity-0">
           <h4 className="leading-none">
             <span className="text-[1.8em]">$00.00</span>
             <span className="text-[1.1em]"> USD</span>
@@ -61,14 +61,14 @@ const SwiperCarouselSkeleton = () => {
         {/* Actions */}
         <div className="flex justify-between pt-[2em] gap-x-[1em]">
           <Button
-            className="text-[1.1em] font-semibold capitalize px-[3em] py-[1em] border border-white rounded-none h-fit bg-white/50"
+            className="text-[1.1em] font-semibold capitalize px-[3em] py-[1em] rounded-none h-fit bg-white"
             disabled
           >
             <p className="opacity-0">Buy now</p>
           </Button>
           <Button
-            variant={"outline"}
-            className="text-[1.1em] font-semibold capitalize px-[3em] py-[1em] rounded-none h-fit bg-white/50"
+            variant="outline"
+            className="text-[1.1em] font-semibold capitalize px-[3em] py-[1em] rounded-none h-fit bg-white border-none"
             disabled
           >
             <p className="opacity-0">Add to cart</p>
